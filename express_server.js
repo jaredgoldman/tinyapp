c456b6c4786819304d0a9f7998f883f837d75a53
jaredgoldmandrums@gmail.com <jaredgoldmandrums@gmail.com>
2021-03-15 18:55:02 +0000

    added ejs as view engine

--- express_server.js
+++ express_server.js
@@ -7,17 +7,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.set('view engine', 'ejs');
+
+app.get("/" , (req, res) => {
+
+  res.render('index', someddatahere);
+
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
