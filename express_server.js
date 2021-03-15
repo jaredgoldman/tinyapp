39dac6634a86541f4047abc17958c1d44217e6b5
jaredgoldmandrums@gmail.com <jaredgoldmandrums@gmail.com>
2021-03-15 19:09:28 +0000

    routed urls_index

--- express_server.js
+++ express_server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
 app.set('view engine', 'ejs');
 
-app.get("/" , (req, res) => {
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
 
-  res.render('index', someddatahere);
+app.get("/urls", (req, res) => {
+  const urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+  };
+  res.render('urls_index', urlDatabase);
 
 });
 
