871d78e9b6f9d780e040158994a92accbd75ab1b
jaredgoldmandrums@gmail.com <jaredgoldmandrums@gmail.com>
2021-03-16 16:40:22 +0000

    added morgan and helper function that helps identify of longUrl has an http prefix

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; // default port 8080'
+const morgan = require('morgan')
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('short'));
+
 
 function generateRandomString(length) {
   const chars = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -20,6 +23,8 @@ const urlDatabase = {
 
 app.set('view engine', 'ejs');
 
+//-------------- ROUTING --------------//
+
 app.get('/', (req, res) => {
   res.send('Hello');
 });
@@ -35,7 +40,9 @@ app.get('/urls/new', (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let newKey = generateRandomString(6);        // Respond with 'Ok' (we will replace this)
+  urlDatabase[newKey] = req.body.longURL;
+  res.redirect(`/urls/${newKey}`); 
 });
 
 app.get('/urls/:shortURL', (req, res) => { 
@@ -43,20 +50,23 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-console.log(generateRandomString(6))
+app.get('/u/:shortUrl', (req, res) => {
+  let longUrl = urlDatabase[req.params.shortUrl]
+  if (!doesUrlHaveHttp(longUrl)) {
+    res.redirect(`http://${longUrl}`)
+  } 
+  res.redirect(longUrl);
+})
 
+const doesUrlHaveHttp = (url) => {
+  return url.startsWith('http');
+}
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+// `http://${req.params.shortUrl}`
+// localhost:8080/u/b2xVn2
+// localhost:8080/
+// localhost:8080/urls
+// localhost:8080/urls/new
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
